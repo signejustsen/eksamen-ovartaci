987181606b5f94c8a7bf71cd8770dfa45acdb106
Paula <paulaengel71@gmail.com>
2026-05-28 21:30:40 +0200

    rettet på knap på favoritside og bibliotek 2
    har ændret font-family på teksterne i knappen

--- JS/timer.js
+++
@@ -1,7 +0,0 @@
-//Loading side timer funktion
-
-// Venter 4 sekunder, før siden går videre
-setTimeout(function () {
-  // Sender brugeren videre til den næste side
-  window.location.href = "din-identitet.html";
-}, 4000);
